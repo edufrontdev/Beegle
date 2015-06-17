2f150f55b18b3992974ad4b9cb97657fc31a9424
edufrontdev <edu@frontdev.cl>
2015-06-17 16:21:06 -0300

    Proyecto Beegle
    Maquetas html+css+js para proyecto de innovación de In Motion, a cargo
    de Esteban Zelada.

---
+++ form.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Beegle</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/master.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#">Project name</a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li role="presentation" class="active"><a href="#">Inicio</a></li>
+					<li role="presentation"><a href="#">Tour</a></li>
+					<li role="presentation"><a href="#">Precios y Planes</a></li>
+					<li role="presentation"><a href="#">Registrarse</a></li>
+					<li role="presentation"><a href="#">Ingresar</a></li>
+				</ul>
+	      </div>
+	    </nav>
+		<div class="espacio"><br><br><br><br></div>
+	    
+
+	    <div class="container">
+	      <!-- Example row of columns -->
+	      <div class="col-md-12">
+	      <form>
+	        <div class="form-group col-md-6">
+	          <label for="exampleInputNombre">Nombres</label>
+	          <input type="text" class="form-control" id="exampleInputNombre" placeholder="Nombre">
+	        </div>
+	        <div class="form-group  col-md-6">
+	          <label for="exampleInputApellido">Apellidos</label>
+	          <input type="text" class="form-control" id="exampleInputApellido" placeholder="Apellido">
+	        </div>
+	        <div class="form-group  col-md-6">
+	          <label for="exampleInputEdad">Edad</label>
+	          <input type="text" class="form-control" id="exampleInputEdad" placeholder="Edad">
+	        </div>
+	        <div class="form-group col-md-6">
+	          <label for="exampleInputEmail1">Email</label>
+	          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+	        </div>
+	        <div class="form-group  col-md-6">
+	          <label for="exampleInputTelefono">Telefono</label>
+	          <input type="text" class="form-control" id="exampleInputTelefono" placeholder="Teléfono">
+	        </div>
+	        <div class="form-group  col-md-6">
+	          <label for="exampleInputPassword1">Password</label>
+	          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+	        </div>
+	        <div class="form-group  col-md-6">
+	          <label for="exampleInputPassword1">Password</label>
+	          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+	        </div>
+	        <div class="form-group  col-md-6">
+	          <label for="exampleInputSelec">Password</label><br>
+	          <select name="selcc" id="sel">
+	          	<option value="1">1</option>
+	          	<option value="2">2</option>
+	          	<option value="3">3</option>
+	          	<option value="4">4</option>
+	          </select>
+	        </div>
+	        
+	        <div class="form-group col-md-6">
+	          <label for="exampleInputFile">File input</label>
+	          <input type="file" id="exampleInputFile">
+	          <p class="help-block">Example block-level help text here.</p>
+	        </div>
+	        <div class="checkbox col-md-6">
+	          <label>
+	            <input type="checkbox"> Check me out
+	          </label>
+	        </div>
+	        <div class="col-md-8">
+	        	<textarea class="form-control" rows="3"></textarea>
+	        </div>
+	        <div class="row">
+	        	<div class="col-md-12">
+	        		<button type="submit" class="btn btn-default">Submit</button>
+	        	</div>
+	        </div>
+	      </form>
+	      </div>
+	      <footer>
+	        <p>&copy; Company 2015</p>
+	      </footer>
+	    </div> <!-- /container -->
+
+
+	    <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	    <!--<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>-->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Beegle</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/master.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#">Project name</a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li role="presentation" class="active"><a href="#">Inicio</a></li>
+					<li role="presentation"><a href="#">Tour</a></li>
+					<li role="presentation"><a href="#">Precios y Planes</a></li>
+					<li role="presentation"><a href="#">Registrarse</a></li>
+					<li role="presentation"><a href="#">Ingresar</a></li>
+				</ul>
+	      </div>
+	    </nav>
+
+	    <!-- Main jumbotron for a primary marketing message or call to action -->
+	    <div class="jumbotron">
+	      <div class="container">
+	        <h1><img src="images/logotipo.svg" alt=""></h1>
+	        <h3>It includes a large callout called a jumbotron</h3>
+	        <p>This is a template for a simple marketing or informational website. Use it as a starting point to create something more unique.</p>
+	        <p><a class="btn btn-success btn-lg" href="#" role="button">PRUEBA NUESTRA APLICACIÓN &raquo;</a></p>
+	      </div>
+	    </div>
+
+	    <div class="container">
+	      <!-- Example row of columns -->
+	      <!-- Three columns of text below the carousel -->
+	            <div class="row" id="caract_size_s">
+	              <div class="col-md-4 col-sm-4 car_s_s">
+	                <img class="img-circle" src="http://lorempixel.com/100/100/technics/1" alt="Generic placeholder image" width="100" height="100">
+	                <h4>Donec sed odio dui.</h4>
+	                <p>Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. </p>
+	                <!-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> -->
+	              </div><!-- /.col-lg-4 -->
+	              <div class="col-md-4 col-sm-4 car_s_s">
+	                <img class="img-circle" src="http://lorempixel.com/100/100/technics/3" alt="Generic placeholder image" width="100" height="100">
+	                <h4>Praesent commodo cursus magna.</h4>
+	                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. </p>
+	                <!-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> -->
+	              </div><!-- /.col-lg-4 -->
+	              <div class="col-md-4 col-sm-4 car_s_s">
+	                <img class="img-circle" src="http://lorempixel.com/100/100/technics/5" alt="Generic placeholder image" width="100" height="100">
+	                <h4>Fusce dapibus, tellus ac cursus commodo</h4>
+	                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.</p>
+	                <!-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> -->
+	              </div><!-- /.col-lg-4 -->
+	            </div><!-- /.row -->
+
+
+	            <!-- START THE FEATURETTES -->
+
+	            <hr class="featurette-divider">
+
+	            <div class="row featurette">
+	              <div class="col-sm-5">
+	                <img class="featurette-image img-responsive center-block" src="http://lorempixel.com/200/200/technics/2" alt="Generic placeholder image">
+	              </div>
+	              <div class="col-sm-7">
+	                <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+	                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+	              </div>
+	            </div>
+
+	            <hr class="featurette-divider">
+
+	            <div class="row featurette">
+	              <div class="col-sm-5 col-sm-push-7">
+	                <img class="featurette-image img-responsive center-block" src="http://lorempixel.com/200/200/technics/4"  alt="Generic placeholder image">
+	              </div>
+	              <div class="col-sm-7 col-sm-pull-5">
+	                <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+	                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+	              </div>
+	            </div>
+
+	            <hr class="featurette-divider">
+
+	            <div class="row featurette">
+	              <div class="col-sm-5">
+	                <img class="featurette-image img-responsive center-block" src="http://lorempixel.com/200/200/technics/6"  alt="Generic placeholder image">
+	              </div>
+	              <div class="col-sm-7">
+	                <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+	                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+	              </div>
+	            </div>
+
+	            <hr class="featurette-divider">
+
+	            <!-- /END THE FEATURETTES -->
+
+
+	      <footer>
+	        <p>&copy; Company 2015</p>
+	      </footer>
+	    </div> <!-- /container -->
+
+
+	    <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	    <!--<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>-->
+</body>
+</html>
